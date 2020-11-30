2561386d8273de7a85802e1edcc22aaffdc62cc5
rutujak963 <rutujak963@gmail.com>
2020-11-30 23:35:18 +0530

    Check Heroku

--- src/app.py
+++ src/app.py
@@ -5,15 +5,14 @@ Created on Mon Nov 30 15:38:40 2020
 @author: Admin
 """
 
-#from flask import Flask
+from flask import Flask
 
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route("/")
+@app.route("/")
 def main():
     return "Welcome to CI Project."
 
-#if __name__ == "__main__":
-#    app.run()
+if __name__ == "__main__":
+    app.run()
 
-main()
