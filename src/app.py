856f583025fbf2118a705819c19fef2e05c29e1e
rutujak963 <rutujak963@gmail.com>
2020-11-30 23:18:53 +0530

    Depoly to Heroku Preset

--- src/app.py
+++ src/app.py
@@ -5,9 +5,14 @@ Created on Mon Nov 30 15:38:40 2020
 @author: Admin
 """
 
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
 def main():
     return "Welcome to CI Project."
 
+if __name__ == "__main__":
+    app.run()
 
-main()
